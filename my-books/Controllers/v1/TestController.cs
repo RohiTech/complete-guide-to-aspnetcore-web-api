8a450b512c536aeb1c69305e2b27644db1450436
Jose Francisco <jfrancisco.fullstackdev@gmail.com>
2022-07-07 12:00:49 -0600

    Http Header-Based Versioning

--- my-books/Controllers/v1/TestController.cs
+++ my-books/Controllers/v1/TestController.cs
@@ -14,19 +14,19 @@ namespace my_books.Controllers.v1
         [HttpGet("get-test-data")]
         public IActionResult GetV1()
         {
-            return Ok("This is TestController V1");
+            return Ok("This is version V1.0");
         }
 
         [HttpGet("get-test-data"), MapToApiVersion("1.2")]
         public IActionResult GetV12()
         {
-            return Ok("This is TestController V1.2");
+            return Ok("This is version V1.2");
         }
 
         [HttpGet("get-test-data"), MapToApiVersion("1.9")]
         public IActionResult GetV19()
         {
-            return Ok("This is TestController V1.9");
+            return Ok("This is version V1.9");
         }
     }
 }
